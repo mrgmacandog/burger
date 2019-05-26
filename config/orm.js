8bfcb47483901310d5972d2714c1cd78255610d4
mrgmacandog <24902881+mrgmacandog@users.noreply.github.com>
2019-05-25 20:59:59 -0700

    Update orm.js methods

--- config/orm.js
+++ config/orm.js
@@ -10,17 +10,38 @@ const orm = {
             if (err) {
                 throw err;
             }
-            
+
             cb(result);
         });
     },
 
-    insertOne: function () {
+    insertOne: function (table, cols, vals, cb) {
+        let questionMarks = "";
+        for (let i = 0; i < vals.length; i++) {
+            if (i < vals.length - 1) {
+                questionMarks += "?, ";
+            } else {
+                questionMarks += "?";
+            }
+        }
 
+        let queryString = `INSERT INTO ${table} (${cols.toString()}) VALUES (${questionMarks});`;
+
+        connection.query(queryString, vals, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
     },
 
-    updateOne: function () {
-
+    updateOne: function (table, objColVals, condition) {
+        /*
+        UPDATE table_name
+        SET devoured = FALSE
+        WHERE id = ?;
+        */
     }
 }
 
