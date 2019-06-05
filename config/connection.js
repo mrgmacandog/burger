5da78e98dcf87ce0881c9e4296fa07bdc766592b
mrgmacandog <24902881+mrgmacandog@users.noreply.github.com>
2019-06-04 23:45:58 -0700

    Add connection to JawsDB for Heroku

--- config/connection.js
+++ config/connection.js
@@ -7,22 +7,29 @@ require("dotenv").config();
 // Import keys dependency
 const keys = require("../keys");
 
+// Initialize the connection
+const connection;
+
 // Instantiate a DB connection
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: keys.rootPassword,
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		host: "localhost",
+		port: 3306,
+		user: "root",
+		password: keys.rootPassword,
+		database: "burgers_db"
+	});
+}
 
 // Make connection
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+connection.connect(function (err) {
+	if (err) {
+		console.error("error connecting: " + err.stack);
+		return;
+	}
+	console.log("connected as id " + connection.threadId);
 });
 
 // Export connection for ORM
